34d968d66d4bb70c5033bca7c7b047f1f075ed3b
Álvaro <alvarorodrigoaj@gmail.com>
2025-05-08 16:44:35 +0200

    Fix: Delete sensible data

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 require('dotenv').config;
 
 module.exports = {
-    DB_USER: process.env.DB_USER || 'alvaroacu',
-    DB_PASS: process.env.DB_PASS || 'pB18CuGiZF5q2Veo',
-    PORT: process.env.PORT || 3000,
-    SECRET_KEY: process.env.SECRET_KEY || 'Bandolero4life',
-    DB_HOST: process.env.DB_HOST || 'localhost'
+    DB_USER: process.env.DB_USER,
+    DB_PASS: process.env.DB_PASS,
+    PORT: process.env.PORT,
+    SECRET_KEY: process.env.SECRET_KEY,
+    DB_HOST: process.env.DB_HOST
 }
