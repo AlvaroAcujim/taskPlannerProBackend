89b499f6e5c0b8371b28b2663f9f6383ab94efb4
Álvaro <alvarorodrigoaj@gmail.com>
2025-05-08 16:04:21 +0200

    Fix: deploy setup

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
     DB_USER: process.env.DB_USER || 'alvaroacu',
     DB_PASS: process.env.DB_PASS || 'pB18CuGiZF5q2Veo',
     PORT: process.env.PORT || 3000,
-    SECRET_KEY: process.env.SECRET_KEY || 'Bandolero4life'
+    SECRET_KEY: process.env.SECRET_KEY || 'Bandolero4life',
+    DB_HOST: process.env.DB_HOST || 'localhost'
 }
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const app = require('./src/app.js');
 const connectDB = require('./src/config/database.js');
 const config = require('./config.js');
 const port = config.PORT;
+const dbhost = config.DB_HOST
 const startServer = async() => {
     try{
         await connectDB();
 
         app.listen(port, () => {
-            console.log(`Servidor corriendo en http://localhost:${port}`);
+            console.log(`Servidor corriendo en http://${dbhost}:${port}`);
         });
     }catch(err){
         console.log('No se ha podido levantar el servidor', err);
