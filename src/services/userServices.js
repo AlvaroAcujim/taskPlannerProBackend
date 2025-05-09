fea70e7ca653c61b4613e08c9748904a81c0c3c7
Álvaro <alvarorodrigoaj@gmail.com>
2025-05-09 14:33:28 +0200

    fix: changed mongo query to handle error

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -67,7 +67,7 @@ const SECRET_KEY = config.SECRET_KEY;
     const loginUser = async (identifier, password) => {
         try {
             const input = identifier.includes('@') ? {email: identifier} : {username: identifier};
-            const user = await User.findOne(input).select('username image events tasks');
+            const user = await User.findOne(input);
             if (!user) {
                 throw new Error('Usuario no encontrado');
             } 
