0377d03a57f03d0138b5affa0e092262e3b64831
Álvaro <alvarorodrigoaj@gmail.com>
2025-05-12 12:53:54 +0200

    Feature: cors modified for create tasks

--- src/app.js
+++ src/app.js
@@ -19,7 +19,9 @@ app.use(cookieParser());
 app.use(cors(
   {
     origin: 'http://127.0.0.1:5500', 
-    credentials: true               
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']              
   }
 ));
 // Protección en cabeceras
